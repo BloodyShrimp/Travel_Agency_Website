c5243ef77e1a6bf0b6371755af932924c16366d5
BloodyShrimp <szymkowerk@gmail.com>
2023-01-06 04:57:36 +0100

    Completed

--- app.js
+++ app.js
@@ -5,3 +5,13 @@ menu.addEventListener("click", function () {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
 });
+
+const form = document.querySelector(".submitted");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const button = document.querySelector('input[type="submit"]');
+  button.disabled = true;
+  const message = document.createElement("p");
+  message.textContent = "Your message has been sent!";
+  button.parentNode.insertBefore(message, button);
+});
